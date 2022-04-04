ed88b04a673b9d25d3c2e4884dc271db11d237dc
Francisco-pixel <64024698+Francisco-pixel@users.noreply.github.com>
2022-04-04 15:37:56 -0400

    Primera subida

---
+++ js/config.js
@@ -0,0 +1,84 @@
+import { c, d } from "https://francisco-pixel.github.io/modulos/js/data.js";
+import { conexionInternet } from "https://francisco-pixel.github.io/modulos/js/conexionInternet.js";
+import { tiempo } from "https://francisco-pixel.github.io/modulos/js/fecha.js";
+import { crearNodo } from "https://francisco-pixel.github.io/modulos/js/crearNodo.js";
+import { loaderStyle } from "https://francisco-pixel.github.io/modulos/js/loader.js";
+import { padreHijo } from "https://francisco-pixel.github.io/modulos/js/padreHijo.js";
+import { setText } from "https://francisco-pixel.github.io/modulos/js/setText.js";
+import { btnUp,btnUpStyle } from "https://francisco-pixel.github.io/modulos/js/botonSubir.js";
+import { copiarText } from "https://francisco-pixel.github.io/modulos/js/copiarText.js";
+import {contacto,
+    estudios,
+    profesion,
+    titulos_izquierda,
+    titulos_derecha,
+    idiomas,
+    habilidades,
+    experiencias} from "./data.js";
+conexionInternet()
+btnUp()
+tiempo()
+copiarText()
+//Títulos de la izquierda
+d.querySelectorAll(".l-aside__contacto").forEach((titulo,i)=>{
+    titulo.innerText=titulos_izquierda[i];
+})
+
+let loop_info_right=(arr,attr)=>{
+    arr.forEach(item=>{
+        d.querySelector(attr).innerHTML+=`
+        <p class="l-aside__contacto-detail">${item}</p>`;
+    })
+}
+loop_info_right(contacto,".contacto")
+loop_info_right(estudios,".estudios")
+loop_info_right(profesion,".profesion")
+
+//Idiomas
+Object.keys(idiomas).forEach((idioma,i)=>{
+    d.querySelector(".idioma").innerHTML+=`
+    <p class="l-aside__contacto-detail">${idioma}</p>
+    <div class="barra">
+        <div class="barra__porciento"></div>
+    </div>
+    `
+    d.querySelectorAll(".barra__porciento")[i].style.width=`0%`;
+    setTimeout(()=>{
+        d.querySelectorAll(".barra__porciento")[i].style.width=`${idiomas[idioma]}%`;
+    },1000)
+})
+
+//Títulos derecha
+d.querySelectorAll(".r-aside-profile__title").forEach((titulo,i)=>{
+    titulo.innerText=titulos_derecha[i];
+})
+//Habilidades
+Object.keys(habilidades).forEach((habilidad,i)=>{
+    d.querySelector(".habilidades").innerHTML+=`
+    <p class="r-aside-profile__p">${habilidad}</p>
+    <div class="barra-r">
+        <div class="barra__porciento-r"></div>
+    </div>
+    `
+    d.querySelectorAll(".barra__porciento-r")[i].style.width=`0%`;
+    setTimeout(()=>{
+        d.querySelectorAll(".barra__porciento-r")[i].style.width=`${habilidades[habilidad]}%`;
+    },1000)
+})
+//Experiencias laborales
+experiencias.forEach(({profesion,institucion,anios,desc})=>{
+    d.querySelector(".experiencia").innerHTML+=`
+    <div class="grid text--align-left-center">
+        <p class="r-aside-profile__p">${anios}</p>
+        <p class="r-aside-profile__p">${institucion}</p>
+    </div>
+    <div class="grid text--align-left-center gap">
+        <h4 class="r-aside-profile__sub-title">${profesion}</h4>
+        <p class="r-aside-profile__p">${desc}</p>
+    </div>
+    `
+})
+    
+d.addEventListener("click",e=>{
+
+})
---
+++ js/data.js
@@ -0,0 +1,35 @@
+export {contacto,
+    estudios,
+    profesion,
+    titulos_izquierda,
+    titulos_derecha,
+    idiomas,
+    habilidades,
+    experiencias};
+let contacto=["(809) 919-5135","francisco-acevedo06@hotmail.com","Calle Primera #45"],
+estudios=["SDQ Training Center, (2019)","UASD, (2018)","ITLA, (2010)","Ramón Emilio Jiménez, (2004)"],
+profesion=["Lic. Diseño Gráfico (UASD)","Desarrollador Web (SDQ)"],
+titulos_izquierda=["Información de Contacto","Estudios","Profesión","Idiomas"],
+titulos_derecha=["Perfil","Experiencia laboral","Habilidades Profesionales"],
+idiomas={
+    "Español":100,
+    "Inglés":60
+},
+habilidades={
+    "JavaScript":80,
+    "HTML":97,
+    "CSS3":90,
+    "PHP":70,
+    "WordPress":95,
+    "Joomla":90,
+    "Illustrator":95,
+    "Photoshop":85,
+    "InDesign":75
+},
+experiencias=[
+    {"profesion":"Diseño Gráfico / Desarrollo Web",
+    "institucion":"PROMESE/CAL",
+    "anios":"2008-2022",
+    "desc":"Laborando desde 04-08-2008, realizando tareas como diseñador de páginas web escribiendo código en distintos lenguajes de programación, tales como: PHP, JavaScript y de maquetación web: HTML5 y CSS3, creando aplicaciones web, mantenimiento al website, subir informaciónes o contenido utilizando Content Management System o CMS como Wordpress y Joomla."
+    }
+];
