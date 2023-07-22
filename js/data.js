3e8f99c44e3e2f7f181f9c18d517d78b27337d4c
Francisco-pixel <franciscoacevedo247@gmail.com>
2023-07-22 18:16:31 -0400

    Update old

---
+++ js/data.js
@@ -0,0 +1,53 @@
+export {contacto,
+    estudios,
+    profesion,
+    titulos_izquierda,
+    titulos_derecha,
+    idiomas,
+    habilidades,
+    experiencias};
+let contacto=["(809) 919-5135","francisco-acevedo06@hotmail.com","Av. Independencia, Santo Domingo, República Dominicana"],
+estudios=["Udemy, (2023)","SDQ Training Center, (2019)"," Universidad Autónoma de Santo Domingo, UASD, (2018)","Instituto Tecnológico de Las Américas, ITLA, (2010)","Ramón Emilio Jiménez, (2004)"],
+profesion=["Lic. Diseño Gráfico, (UASD)","Desarrollo Web (SDQ)"],
+titulos_izquierda=["Información de Contacto","Estudios","Profesión","Idiomas"],
+titulos_derecha=["Perfil","Experiencia laboral","Habilidades Profesionales"],
+idiomas={
+    "Español":100,
+    "Inglés":60
+},
+habilidades={
+    "JavaScript":80,
+    "HTML5":97,
+    "CSS3":90,
+    "PHP":70,
+    "React":40,
+    "NodeJs":35,
+    "Git":65,
+    "Firebase":60,
+    "WordPress":95,
+    "Joomla":90,
+    "Illustrator":95,
+    "Photoshop":85,
+    "InDesign":75
+},
+experiencias=[
+    {"profesion":"Desarrollo Web",
+    "institucion":"IDEICE",
+    "anios":"2022",
+    "desc":`Laborando desde 01-08-2022 en el Departamento de TIC, realizando tareas como desarrollador web tales como: mantenimiento 
+    y desarrollo de módulos de la intranet institucional,
+    creación de aplicaciones web, soporte técnico en la interfaz de usuario de la Revista de Investigación y Evaluación Educativa (revie), y mantenimiento del sitio web institucional, Observatorio de Políticas Educativas de la 
+    República Dominicana (OPERD).`
+    },
+    {"profesion":"Diseño Gráfico / Desarrollo Web",
+    "institucion":"PROMESE/CAL",
+    "anios":"2008-2022",
+    "desc":`Laborando desde 04-08-2008, realizando tareas de diseñador 
+    de páginas web escribiendo código en distintos lenguajes de programación, 
+    tales como: PHP, JavaScript y de maquetación web: HTML5 y CSS3, creando 
+    aplicaciones web, mantenimiento al website, crear los artes para las redes sociales y sitio web institucional,
+    subir informaciones o contenido 
+    utilizando Content Management System (CMS) como Wordpress o Joomla y he trabajado 
+    en la adquisición de las Normas sobre Tenologías de la Información (NORTIC) A2 y A3.`
+    }
+];
